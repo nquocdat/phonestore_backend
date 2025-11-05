a93189f9cd524dac5e71861aefd10f7939c78c7e
Nguyen Quoc Dat <dnguyenquoc45@gmail.com>
2025-11-06 01:13:38 +0700

    tao du an moi

---
+++ src/main/java/com/example/phonestore_backend/PhonestoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.phonestore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonestoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonestoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phonestore_backend/PhonestoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phonestore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonestoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
